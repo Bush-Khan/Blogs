886e29ef31cc1c537a4fb12ee71d5620654ff2ba
shivani-0515 <shivanichaudhary05072001@gmail.com>
2022-06-21 14:10:13 +0530

    modified

---
+++ src/route/route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+const authorController = require("../controller/authorController")
+
+router.post("/author",authorController.createAuthor)
+
+module.exports = router
